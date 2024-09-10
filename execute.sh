0d98eb9f7f1a17ceeb2c1d18e3860841e8e542df
keirawu011029 <1056674151@qq.com>
2024-09-10 19:41:29 +0800

    excecute

---
+++ execute.sh
@@ -0,0 +1,21 @@
+# python train_cifar10.py --net vit_timm --dp --lr 0.01 --nowandb
+
+# python train_cifar10.py --net vit_timm --dp --lr 0.05 --nowandb
+
+# python train_cifar10.py --net vit_timm --dp --lr 0.1 --nowandb
+
+# python train_cifar10.py --net vit_timm --dp --lr 0.2 --nowandb
+
+# python train_cifar10.py --net vit_timm --dp --lr 0.02 --nowandb
+# python train_cifar10.py --net vit_timm --dp --lr 0.03 --nowandb
+
+# python train_cifar10.py --net vit_timm --dp --lr 0.04 --nowandb
+
+
+# python train_cifar10.py --net vit_timm --dp --lr 0.001 --nowandb
+
+# python train_cifar10.py --net vit_timm --dp --lr 0.005 --nowandb
+
+# python train_cifar10.py --net vit_timm --dp --lr 1e-4 --nowandb
+# python train_cifar10.py --net vit_timm --dp --lr 5e-4 --nowandb
+python train_cifar10.py --net vit_timm --dp --lr 0.035 --nowandb
